34a4fb6aea02b9d940f248b9f376be5d6d59d52d
Artiom Buga <Artiom.Buga@endava.com>
2023-06-30 12:55:07 +0300

    Fixed Incorrect error message when user with no data tries to sign in and Incorrect Error Message when user with invalid email tries to sign in

--- src/main/java/com/endava/parkinglot/DTO/auth/AuthenticationDTO.java
+++ src/main/java/com/endava/parkinglot/DTO/auth/AuthenticationDTO.java
@@ -1,17 +1,23 @@
 package com.endava.parkinglot.DTO.auth;
 
+import jakarta.validation.GroupSequence;
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
 @Builder
+@GroupSequence({AuthenticationDTO.NotEmptyValidationGroup.class, AuthenticationDTO.PatternValidationGroup.class, AuthenticationDTO.SizeValidationGroup.class, AuthenticationDTO.EmailValidationGroup.class, AuthenticationDTO.class})
 public class AuthenticationDTO {
 
-    @NotEmpty(message = "Email cannot be empty !")
-    @Size(min = 12, max = 35, message = "Size of the email should be between 12 and 35 characters !")
-    @Email(message = "")
+    @NotBlank(message = "Email cannot be empty.", groups = NotEmptyValidationGroup.class)
+    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$", message="Invalid email. It should be like: 'example@email.com'.", groups = PatternValidationGroup.class)
+    @Size(min = 12, max = 35, message = "Size of the email should be between 12 and 35 characters.", groups = SizeValidationGroup.class)
+    @Email(message = "Please enter a valid email address.", groups = EmailValidationGroup.class)
     private String email;
+
+    @NotBlank(message = "Password cannot be empty.", groups = NotEmptyValidationGroup.class)
     private String password;
 
     public String getEmail() {
@@ -28,5 +34,19 @@ public class AuthenticationDTO {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public interface NotEmptyValidationGroup {
+    }
+
+    public interface SizeValidationGroup {
+    }
+
+    public interface EmailValidationGroup{
+
+    }
+
+    public interface PatternValidationGroup{
+
     }
 }
